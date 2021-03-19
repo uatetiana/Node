0333d3130cde1455dabe2db5baf39a9ededa07f7
Tetiana Hrebeniuk <tetiana.dnipro@gmail.com>
2021-03-19 18:09:28 +0200

    hw_1 task 1,2

--- HW_1/js/main.js
+++ HW_1/js/main.js
@@ -40,3 +40,44 @@ convertToBinary(1234);
 // "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
 // "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
 
+function sortString(string) {
+  let arr = string.split(' ');
+
+  let sorted = [];
+  if (string.length !== 0) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        for (let k = 0; k < arr[j].length; k++) {
+          if (+arr[j][k] === i+1) {
+            sorted[i] = arr[j];
+          }
+        }
+      }
+    }
+    console.log(sorted.join(' '));
+    return sorted.join(' ');
+  } else {
+    console.log('');
+    return '';
+  }
+}
+
+sortString("Fo1r the2 g3ood 4of th5e pe6ople")
+
+// Football Task:
+//
+// Most football fans love it for the goals and excitement.
+// You are to handle the referee's little notebook and count the players who were sent off for fouls and misbehavior.
+// The rules: Two teams, named "A" and "B" have 11 players each; players on each team are numbered from 1 to 11.
+// Any player may be sent off the field by being given a red card. A player can also receive a yellow warning card,
+// which is fine, but if he receives another yellow card, he is sent off immediately (no need for a red card in that case).
+// If one of the teams has less than 7 players remaining, the game is stopped immediately by the referee, and the team with less than 7 players loses.
+//
+// A card is a string with the team's letter ('A' or 'B'), player's number, and card's color ('Y' or 'R') -
+// all concatenated and capitalized. e.g the card 'B7Y' means player #7 from team B received a yellow card.
+
+// The task: Given a list of cards (could be empty),
+// return the number of remaining players on each team at the end of the game (as a tuple of 2 integers, team "A" first).
+// If the game was terminated by the referee for insufficient number of players, you are to stop the game immediately, and ignore any further possible cards.
+//
+// If a player that has already been sent off receives another card - ignore it.
