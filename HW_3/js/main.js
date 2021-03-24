712e10bacccd664edb54ddc3d948b4675b76ffca
Tetiana Hrebeniuk <tetiana.dnipro@gmail.com>
2021-03-24 11:49:19 +0200

    HW_3

--- HW_3/js/main.js
+++ HW_3/js/main.js
@@ -1,58 +1,60 @@
 // task #1 --- pop
-// function myPop(arr) {
-//   if (arr.length !== 0) {
-//     let item = arr[arr.length - 1];
-//     arr.length = arr.length - 1;
-//     return item;
-//   } else {
-//     return undefined;
-//   }
-// }
-//
-// let arr1 = [1, 4, 6]
-// let a = myPop(arr1);
-// console.log(a);
-// console.log(arr1);
-//
-// let myFish = ['ангел', 'клоун', 'мандарин', 'хирург'];
-// console.log(myPop(myFish));
-// console.log(myPop(myFish));
-// console.log(myPop(myFish));
-// console.log(myPop(myFish));
-// console.log(myPop(myFish));
+function myPop(arr) {
+  if (arr.length !== 0) {
+    let item = arr[arr.length - 1];
+    arr.length = arr.length - 1;
+    return item;
+  } else {
+    return undefined;
+  }
+}
+
+let arr1 = [1, 4, 6];
+let a = myPop(arr1);
+console.log(a);
+console.log(arr1);
+
+let myFish = ["ангел", "клоун", "мандарин", "хирург"];
+console.log(myPop(myFish));
+console.log(myPop(myFish));
+console.log(myPop(myFish));
+console.log(myPop(myFish));
+console.log(myPop(myFish));
 
 //task #2 --- push
-// function myPush(arr, ...args) {
-//   for (let i = 1 ; i < arguments.length; i++) {
-//       arr[arr.length] = arguments[i];
-//   }
-//   return arr.length;
-// }
-//
-// const vegetables = ['пастернак', 'картошка'];
-// const moreVegs = ['сельдерей', 'капуста'];
-// const word = 'hello';
-// console.log(myPush(vegetables, moreVegs));
-// console.log(myPush(vegetables, word, 'bye'));
-// console.log(myPush(vegetables, 1, [2, 3]));
-// console.log(myPush(vegetables, 1, {hello: 'hello'}));
-// console.log(vegetables)
+function myPush(arr, ...args) {
+  for (let i = 1; i < arguments.length; i++) {
+    arr[arr.length] = arguments[i];
+  }
+  return arr.length;
+}
 
-// function myShift(arr) {
-//   if (arr.length !== 0) {
-//     arr.reverse();
-//     let item = arr[arr.length - 1];
-//     arr.length = arr.length - 1;
-//     arr.reverse()
-//     return item;
-//   } else {
-//     return undefined;
-//   }
-// }
-// let shiftArr = [3, 7, 10, 'hello', '555'];
-// console.log(myShift(shiftArr));
-// console.log(shiftArr);
+const vegetables = ["пастернак", "картошка"];
+const moreVegs = ["сельдерей", "капуста"];
+const word = "hello";
+console.log(myPush(vegetables, moreVegs));
+console.log(myPush(vegetables, word, "bye"));
+console.log(myPush(vegetables, 1, [2, 3]));
+console.log(myPush(vegetables, 1, { hello: "hello" }));
+console.log(vegetables);
 
+//task #3 --- shift
+function myShift(arr) {
+  if (arr.length !== 0) {
+    arr.reverse();
+    let item = arr[arr.length - 1];
+    arr.length = arr.length - 1;
+    arr.reverse();
+    return item;
+  } else {
+    return undefined;
+  }
+}
+let shiftArr = [3, 7, 10, "hello", "555"];
+console.log(myShift(shiftArr));
+console.log(shiftArr);
+
+//task #4 --- unshift
 function myUnshift(arr, param) {
   arr.reverse();
   arr[arr.length] = param;
@@ -64,6 +66,7 @@ let unshiftArr = [120, 15, 17, 35];
 console.log(myUnshift(unshiftArr, 1));
 console.log(unshiftArr);
 
+//task #5 --- concat
 function myConcat(arr, ...args) {
   let newArr = [...arr];
 
@@ -91,20 +94,19 @@ console.log(myConcat(concatArr, 1, { hello: "hello" }));
 console.log(concatArr);
 
 let mapArr = ["Яблоко", "Банан", "Aнанас"];
-
 //const resMarArr = mapArr.map(el => el[0]);
 //console.log(resMarArr);
+//const filterArr = mapArr.filter((el) => el[0].toLowerCase() == "a");
+//console.log(filterArr);
+//let mapArr1 = ["Яблоко", "Банан", "Aнанас"];
+//mapArr1.forEach((el, i, arr) => (arr[i] = `${i + 1}: ${el}`));
+//console.log(mapArr1);
 
 //Map TO REDUCE
-
 const resReduceArr = mapArr.reduce((acc, el) => {
   return [...acc, el.slice(0, 1)];
 }, []);
-
 console.log(resReduceArr);
-
-//const filterArr = mapArr.filter((el) => el[0].toLowerCase() == "a");
-//console.log(filterArr);
 
 //Filter TO REDUCE
 const resReduceArrFilt = mapArr.reduce((acc, el) => {
@@ -112,18 +114,12 @@ const resReduceArrFilt = mapArr.reduce((acc, el) => {
     return [el];
   }
 }, []);
-
 console.log(resReduceArrFilt);
-
-//let mapArr1 = ["Яблоко", "Банан", "Aнанас"];
-//mapArr1.forEach((el, i, arr) => (arr[i] = `${i + 1}: ${el}`));
-//console.log(mapArr1);
 
 //FOR EACH TO REDUCE
 let mapArr2 = ["Яблоко", "Банан", "Aнанас"];
 const res = mapArr2.reduce((acc, el, index, arr) => {
   return [...acc, `${index + 1}: ${arr[index]}`];
 }, []);
-
 console.log(mapArr2);
 console.log(res);
