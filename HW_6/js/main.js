f131fcfc2a757c1c4c2a959b08513f701283375c
Tetiana Hrebeniuk <tetiana.dnipro@gmail.com>
2021-03-29 13:31:46 +0300

    hw_6

---
+++ HW_6/js/main.js
@@ -0,0 +1,86 @@
+function gameMain() {
+  let board = getBoard();
+  let player1 = checkVictory(board, 1),
+    player2 = checkVictory(board, 2);
+  switch (true) {
+    case player1: {
+      alert(1);
+      break;
+    }
+    case player2: {
+      alert(2);
+      break;
+    }
+    case !(player1 || player2) && checkZeroes(board): {
+      alert(-1);
+      break;
+    }
+    default: {
+      alert(0);
+      break;
+    }
+  }
+}
+
+function getCell(cell = "") {
+  while (cell != "0" && cell != "1" && cell != "2") {
+    cell = prompt("Enter the cell data");
+  }
+  return cell;
+}
+
+function getBoard() {
+  let board = [[], [], []];
+  for (let row = 0; row < 3; row++) {
+    for (let col = 0; col < 3; col++) {
+      board[row][col] = getCell();
+    }
+  }
+  return board;
+}
+
+function checkRows(board, player) {
+  return (
+    board[0].every((elem) => elem == player) ||
+    board[1].every((elem) => elem == player) ||
+    board[2].every((elem) => elem == player)
+  );
+}
+
+function checkColumns(board, player) {
+  let col1 = board.map((elem) => elem[0]),
+    col2 = board.map((elem) => elem[1]),
+    col3 = board.map((elem) => elem[2]);
+  return (
+    col1.every((elem) => elem == player) ||
+    col2.every((elem) => elem == player) ||
+    col3.every((elem) => elem == player)
+  );
+}
+
+function checkDiagonals(board, player) {
+  let diag1 = board.map((elem, index) => elem[index]),
+    diag2 = board.map((elem, index) => elem[2 - index]);
+  return (
+    diag1.every((elem) => elem == player) ||
+    diag2.every((elem) => elem == player)
+  );
+}
+
+function checkVictory(board, player) {
+  return (
+    checkRows(board, player) ||
+    checkColumns(board, player) ||
+    checkDiagonals(board, player)
+  );
+}
+
+function checkZeroes(board) {
+  return (
+    board[0].some((element) => element == 0) ||
+    board[1].some((element) => element == 0) ||
+    board[2].some((element) => element == 0)
+  );
+}
+
+gameMain();
